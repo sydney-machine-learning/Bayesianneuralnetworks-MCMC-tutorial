a9f6dc7c03f5c5f054d15aa4c8ba7e222d680be5
RC <c.rohitash@gmail.com>
2022-06-14 12:58:15 +1000

    simple MCMC updated

--- simple-MCMC/MCMC.py
+++ simple-MCMC/MCMC.py
@@ -32,11 +32,12 @@ p = np.random.uniform(0, 1)
 n_samples = 500
 burn_in = 50
  
-
+count = 0 #count accepted samples
 # Create the MCMC loop
 for i in range(n_samples):
     # Propose a new value of p randomly from a uniform distribution between 0 and 1
     p_new = np.random.random_sample()
+    print(i, p_new)
     # Compute acceptance probability
     R = acceptance_ratio(p, p_new)
     # Draw random sample to compare R to
@@ -44,11 +45,15 @@ for i in range(n_samples):
     # If R is greater than u, accept the new value of p (set p = p_new)
     if u < R:
         p = p_new
+        count = count +1
     # Record values after burn in 
     if i > burn_in:
         results.append(p)
 print(results, 'results')
-
+per_accept = (count/n_samples)*100
+print(per_accept, 'percentage accept')
 pos_mean = np.mean(results)
+
+# Royce - give histogram and traceplot of results - improve comments and variable names
 
 print(pos_mean)
